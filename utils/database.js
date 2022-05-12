ed18263a19c7fb2238e0fd10b93c711731c3fb51
Roberto Rodriguez <rdrgzmndz.sm0101@gmail.com>
2022-05-11 21:47:45 -0500

    5th commit

--- utils/database.js
+++ utils/database.js
@@ -11,7 +11,7 @@ const db = new Sequelize({
   password: process.env.DB_PASSWORD,
   database: process.env.DB,
   logging: false,
-  dialectOptions: process.env.NODE_ENV === production ? {
+  dialectOptions: process.env.NODE_ENV === "production" ? {
     ssl: {
       require: true,
       rejectUnauthorized: false
